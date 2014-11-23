ee787bd13529aaeea9fe67a1a1e0d62c1109dcb9
Andreas Schneider <asn@cryptomilk.org>
2014-11-23 19:24:11 +0100

    device: Add camera support.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,14 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/libbt_vndcfg.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
+### CAMERA
+# frameworks/av/services/camera/libcameraservice
+BOARD_NEEDS_MEMORYHEAPION := true
+# hardware/samsung/exynos/libcsc
+BOARD_USES_SCALER := true
+# frameworks/av/camera, camera blob support
+COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
+
 # CHARCHER
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 BOARD_BATTERY_DEVICE_NAME := battery
