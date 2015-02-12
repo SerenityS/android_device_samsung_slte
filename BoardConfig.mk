09a2ef3e40765775a6d39a2a3c65f52a9497876e
Andreas Schneider <asn@cryptomilk.org>
2015-02-12 15:43:22 +0100

    sepolicy: Allow vold to write fscklogs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,8 +119,11 @@ BOARD_SEPOLICY_DIRS := \
 
 BOARD_SEPOLICY_UNION := \
 	app.te \
+	device.te \
+	domain.te \
 	file_contexts \
 	file.te \
+	vold.te \
 	uventd.te
 
 ###########################################################
