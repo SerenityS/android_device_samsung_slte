9f9c0d221775df2d060f24317d727854f84e53be
Andreas Schneider <asn@cryptomilk.org>
2015-03-23 18:56:08 +0100

    BoardConfig: Updater charger settings

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,8 +100,13 @@ BOARD_USES_SCALER := true
 COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
 
 # CHARCHER
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 BOARD_BATTERY_DEVICE_NAME := battery
+# system/core/init/Android.mk
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+# system/core/healthd/Android.mk
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
 
 ### RIL
 BOARD_VENDOR := samsung
