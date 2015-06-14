d9079d7195f447ae04f71cfb52f64530311aed56
kyasu <kyasu.m2@gmail.com>
2015-06-14 23:46:58 +0900

    Add TWRP definitions

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,10 +31,6 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2401239040
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 28219277312
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# FIMG2D
-BOARD_USES_SKIA_FIMGAPI := true
-BOARD_USES_NEON_BLITANTIH := true
-
 # Kernel
 #TARGET_PREBUILT_KERNEL := device/samsung/slte/kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/slte
@@ -60,30 +56,16 @@ BOARD_USES_LIBMEDIA_WITH_AUDIOPARAMETER := true
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
+# Bionic (Enable QC's libm optimizations)
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/libbt_vndcfg.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
-# Camera
-# COMMON_GLOBAL_CFLAGS += -DUSE_MEMORY_HEAP_ION
-BOARD_NEEDS_MEMORYHEAPION := true
-COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_DVFS
-
-# Media
-#COMMON_GLOBAL_CFLAGS += -DUSE_NATIVE_SEC_NV12TILED # use format from fw/native
-COMMON_GLOBAL_CFLAGS += -DWIDEVINE_PLUGIN_PRE_NOTIFY_ERROR
-
-#COMMON_GLOBAL_CFLAGS += -DTARGET_ENABLE_QC_AV_ENHANCEMENTS
-#BOARD_USE_SAMSUNG_COLORFORMAT := true
-
-# Scaler
-BOARD_USES_SCALER := true
-
-# Charger
+# Battery
 BOARD_BATTERY_DEVICE_NAME := battery
 # system/core/init/Android.mk
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
@@ -92,14 +74,44 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
 
+# Camera
+# COMMON_GLOBAL_CFLAGS += -DUSE_MEMORY_HEAP_ION
+BOARD_NEEDS_MEMORYHEAPION := true
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DSAMSUNG_DVFS
+
+# FIMG2D
+BOARD_USES_SKIA_FIMGAPI := true
+BOARD_USES_NEON_BLITANTIH := true
+
 # Graphics
 USE_OPENGL_RENDERER := true
 BOARD_EGL_CFG := $(LOCAL_PATH)/configs/egl/egl.cfg
 BOARD_HDMI_INCAPABLE := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 5
 
-# Enable QC's libm optimizations
-TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+# HWCServices
+#BOARD_USES_HWC_SERVICES := true
+
+# Hardware
+BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# Init
+TARGET_NR_SVC_SUPP_GIDS := 20
+
+# Media
+COMMON_GLOBAL_CFLAGS += -DUSE_NATIVE_SEC_NV12TILED # use format from fw/native
+COMMON_GLOBAL_CFLAGS += -DWIDEVINE_PLUGIN_PRE_NOTIFY_ERROR
+#COMMON_GLOBAL_CFLAGS += -DTARGET_ENABLE_QC_AV_ENHANCEMENTS
+#BOARD_USE_SAMSUNG_COLORFORMAT := true
+
+# Nfc
+BOARD_NFC_CHIPSET := pn547
+BOARD_NFC_HAL_SUFFIX := universal5430
 
 # OpenMAX Video
 BOARD_USE_STOREMETADATA := true
@@ -116,37 +128,16 @@ BOARD_USE_VP8ENC_SUPPORT := true
 BOARD_USE_ENCODER_RGBINPUT_SUPPORT := true
 BOARD_USE_DUALDPB_MODE := true
 
-# Include an expanded selection of fonts
-EXTENDED_FONT_FOOTPRINT := true
-
-# Hardware
-BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw
-
 # PowerHAL
 TARGET_POWERHAL_VARIANT := slte
-
-# Nfc
-BOARD_NFC_CHIPSET := pn547
-BOARD_NFC_HAL_SUFFIX := universal5430
 
 # RIL
 BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm7260
 BOARD_RIL_CLASS := ../../../device/samsung/slte/ril
 
-# Wifi
-BOARD_HAVE_SAMSUNG_WIFI := true
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
-BOARD_WLAN_DEVICE                := bcmdhd
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_NVRAM_PATH_PARAM     := "/sys/module/dhd/parameters/nvram_path"
-WIFI_DRIVER_NVRAM_PATH           := "/etc/wifi/nvram_net.txt"
-WIFI_DRIVER_FW_PATH_STA          := "/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/etc/wifi/bcmdhd_apsta.bin"
+# Scaler
+BOARD_USES_SCALER := true
 
 # Selinux
 BOARD_SEPOLICY_DIRS := \
@@ -169,12 +160,23 @@ BOARD_USES_SYNC_MODE_FOR_MEDIA := true
 # Webkit
 ENABLE_WEBGL := true
 
-# Init
-TARGET_NR_SVC_SUPP_GIDS := 20
-
 # WFD
 BOARD_USES_WFD_SERVICE := true
 BOARD_USES_WFD := true
+
+# Wifi
+BOARD_HAVE_SAMSUNG_WIFI          := true
+BOARD_WLAN_DEVICE                := bcmdhd
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_NVRAM_PATH_PARAM     := "/sys/module/dhd/parameters/nvram_path"
+WIFI_DRIVER_NVRAM_PATH           := "/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_FW_PATH_STA          := "/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/etc/wifi/bcmdhd_apsta.bin"
 
 # Recovery
 #TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.universal5430
@@ -186,6 +188,19 @@ BOARD_HAS_NO_REAL_SDCARD := true
 BOARD_RECOVERY_SWIPE := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 RECOVERY_SDCARD_ON_DATA := true
+
+# TWRP Recovery
+#TARGET_RECOVERY_DEVICE_MODULES += prebuilt_file_contexts exyrngd
+TW_THEME := portrait_mdpi
+TW_BRIGHTNESS_PATH := /sys/class/backlight/panel/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_INCLUDE_JB_CRYPTO := true
+TW_INCLUDE_L_CRYPTO := true
+# The kernel has exfat support.
+TW_NO_EXFAT_FUSE := true
+#TW_DISABLE_TTF := true
 
 # inherit from the proprietary version
 -include vendor/samsung/slte/BoardConfigVendor.mk	
