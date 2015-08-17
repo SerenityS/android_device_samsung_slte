4e2d0345d6335f84ae57c98e87fd3c50379200e8
Christopher N. Hesse <raymanfx@gmail.com>
2015-08-18 00:06:47 +0200

    slteskt: Allow capacitive buttons to be disabled
    Change-Id: Ie6ac67f1f63dcd60afa7787dc85a4dce854b28a0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -240,6 +240,7 @@ EXTENDED_FONT_FOOTPRINT := true
 
 ### CMHW
 BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
+BOARD_HARDWARE_CLASS += device/samsung/slteskt/cmhw
 
 ### SELINUX
 BOARD_SEPOLICY_DIRS := \
