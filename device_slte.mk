222a59f15787531b1cabbeb98ac5a2270f00c336
Andreas Schneider <asn@cryptomilk.org>
2015-08-21 10:00:36 +0200

    slte: Use AwesomePlayer by default

--- device_slte.mk
+++ device_slte.mk
@@ -188,6 +188,10 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
 	$(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
 
+# Some Exynos HW codecs require AwesomePlayer
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.media.use-awesome=true
+
 ###########################################################
 ### POWER
 ###########################################################
