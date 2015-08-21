b3e09b5ae15a3c78e7dfa8389bf64caf02b7b2e3
Andreas Schneider <asn@cryptomilk.org>
2015-08-21 11:51:47 +0200

    config: Add supl TLS support to gps config

--- device_slte.mk
+++ device_slte.mk
@@ -219,6 +219,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
+	$(LOCAL_PATH)/configs/gps/SuplRootCert:system/etc/SuplRootCert \
 	$(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
 
 ###########################################################
