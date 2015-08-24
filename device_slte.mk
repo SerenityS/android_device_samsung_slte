e1d254560ff342395133e006f2a42da1800bdb2d
Andreas Schneider <asn@cryptomilk.org>
2015-08-24 21:04:05 +0200

    config: Add CM default audio_effects.conf

--- device_slte.mk
+++ device_slte.mk
@@ -145,8 +145,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ### AUDIO
 ###########################################################
 
-# $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf
 PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
 	$(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
 	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
