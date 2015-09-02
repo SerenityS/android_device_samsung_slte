5b8c43a01352097d68f7bfe78773c8c0c261605c
Andreas Schneider <asn@cryptomilk.org>
2015-09-02 11:38:14 +0200

    cm: Set additional build information

--- cm.mk
+++ cm.mk
@@ -13,6 +13,11 @@ $(call inherit-product, device/samsung/slte/device_slte.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_slte
 PRODUCT_DEVICE := slte
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-G850F
-PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=sltexx \
+	PRODUCT_MODEL=SM-G850F \
+	PRODUCT_MANUFACTURER=samsung \
+	TARGET_DEVICE=slte \
+	BUILD_FINGERPRINT=samsung/sltexx/slte:5.0.2/LRX22G/G850FXXU2COD4:user/release-keys \
+	PRIVATE_BUILD_DESC="sltexx-user 5.0.2 LRX22G G850FXXU2COD4 release-keys"
