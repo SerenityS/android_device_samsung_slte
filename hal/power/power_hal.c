3cd040d9e28104f2e172e46d6887c0192fb3e729
Andreas Schneider <asn@cryptomilk.org>
2015-10-29 11:40:25 +0100

    WIP powerhal: Implement performance profiles

--- hal/power/power_hal.c
+++ hal/power/power_hal.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014 The Android Open Source Project
- * Copyright (c) 2014 Andreas Schneider <asn@cryptomilk.org>
+ * Copyright (c) 2014-2015 Andreas Schneider <asn@cryptomilk.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/time.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 #define LOG_TAG "PowerHAL"
@@ -71,8 +72,19 @@ static unsigned int vsync_count;
 static struct timespec last_touch_boost;
 static bool touch_boost;
 
-/* POWER_HINT_LOW_POWER */
+/* FIXME POWER_HINT_LOW_POWER */
 static bool low_power_mode = false;
+
+enum power_profile_e {
+    PROFILE_POWER_SAVE = 0,
+    PROFILE_BALANCED,
+    PROFILE_HIGH_PERFORMANCE
+};
+static enum power_profile_e current_power_profile = PROFILE_BALANCED;
+
+/**********************************************************
+ *** HELPER FUNCTIONS
+ **********************************************************/
 
 static void sysfs_write(const char *path, char *s)
 {
@@ -95,6 +107,98 @@ static void sysfs_write(const char *path, char *s)
 
     close(fd);
 }
+
+static struct timespec timespec_diff(struct timespec lhs, struct timespec rhs)
+{
+    struct timespec result;
+    if (rhs.tv_nsec > lhs.tv_nsec) {
+        result.tv_sec = lhs.tv_sec - rhs.tv_sec - 1;
+        result.tv_nsec = NSEC_PER_SEC + lhs.tv_nsec - rhs.tv_nsec;
+    } else {
+        result.tv_sec = lhs.tv_sec - rhs.tv_sec;
+        result.tv_nsec = lhs.tv_nsec - rhs.tv_nsec;
+    }
+    return result;
+}
+
+/**********************************************************
+ *** POWER FUNCTIONS
+ **********************************************************/
+
+static int check_boostpulse_on(struct timespec diff)
+{
+    long boost_ns = (BOOST_PULSE_DURATION * NSEC_PER_USEC) % NSEC_PER_SEC;
+    long boost_s = BOOST_PULSE_DURATION / USEC_PER_SEC;
+
+    if (diff.tv_sec == boost_s)
+        return (diff.tv_nsec < boost_ns);
+    return (diff.tv_sec < boost_s);
+}
+
+/* You need to request the powerhal lock before calling this function */
+static int boostpulse_open(struct exynos5430_power_module *exynos5430_pwr)
+{
+    char errno_str[64];
+
+    if (exynos5430_pwr->boostpulse_fd < 0) {
+        exynos5430_pwr->boostpulse_fd = open(BOOSTPULSE_PATH, O_WRONLY);
+        if (exynos5430_pwr->boostpulse_fd < 0) {
+            if (!exynos5430_pwr->boostpulse_warned) {
+                strerror_r(errno, errno_str, sizeof(errno_str));
+                ALOGE("Error opening %s: %s\n", BOOSTPULSE_PATH, errno_str);
+                exynos5430_pwr->boostpulse_warned = 1;
+            }
+        }
+    }
+
+    return exynos5430_pwr->boostpulse_fd;
+}
+
+static void set_power_profile(enum power_profile_e profile)
+{
+    int rc;
+    struct stat sb;
+
+    if (current_power_profile == profile) {
+        return;
+    }
+
+    ALOGV("%s: profile=%d", __func__, profile);
+
+    switch (profile) {
+        case PROFILE_POWER_SAVE:
+            sysfs_write(CPU0_MAX_FREQ_PATH, LOW_POWER_MAX_FREQ);
+            rc = stat(CPU4_MAX_FREQ_PATH, &sb);
+            if (rc == 0) {
+                sysfs_write(CPU4_MAX_FREQ_PATH, LOW_POWER_MAX_FREQ);
+            }
+
+            ALOGD("%s: set powersave mode", __func__);
+            break;
+        case PROFILE_BALANCED:
+            sysfs_write(CPU0_MAX_FREQ_PATH, NORMAL_MAX_FREQ);
+            rc = stat(CPU4_MAX_FREQ_PATH, &sb);
+            if (rc == 0) {
+                sysfs_write(CPU4_MAX_FREQ_PATH, NORMAL_MAX_FREQ);
+            }
+            ALOGD("%s: set balanced mode", __func__);
+            break;
+        case PROFILE_HIGH_PERFORMANCE:
+            sysfs_write(CPU0_MAX_FREQ_PATH, NORMAL_MAX_FREQ);
+            rc = stat(CPU4_MAX_FREQ_PATH, &sb);
+            if (rc == 0) {
+                sysfs_write(CPU4_MAX_FREQ_PATH, NORMAL_MAX_FREQ);
+            }
+            ALOGD("%s: set performance mode", __func__);
+            break;
+    }
+
+    current_power_profile = profile;
+}
+
+/**********************************************************
+ *** INIT FUNCTIONS
+ **********************************************************/
 
 static void init_touchscreen_power_path(struct exynos5430_power_module *exynos5430_pwr)
 {
@@ -156,9 +260,9 @@ done:
 }
 
 /*
- * This function performs power management setup actions at runtime startup,
- * such as to set default cpufreq parameters.  This is called only by the Power
- * HAL instance loaded by PowerManagerService.
+ * The init function performs power management setup actions at runtime
+ * startup, such as to set default cpufreq parameters.  This is called only by
+ * the Power HAL instance loaded by PowerManagerService.
  */
 static void exynos5430_power_init(struct power_module *module)
 {
@@ -228,12 +332,28 @@ out:
     init_touchkey_power_path(exynos5430_pwr);
 }
 
-/* This function performs power management actions upon the system entering
- * interactive state (that is, the system is awake and ready for interaction,
- * often with UI devices such as display and touchscreen enabled) or
- * non-interactive state (the system appears asleep, display usually turned
- * off).  The non-interactive state is usually entered after a period of
- * inactivity, in order to conserve battery power during such inactive periods.
+/*
+ * The setInteractive function performs power management actions upon the
+ * system entering interactive state (that is, the system is awake and ready
+ * for interaction, often with UI devices such as display and touchscreen
+ * enabled) or non-interactive state (the system appears asleep, display
+ * usually turned off).  The non-interactive state is usually entered after a
+ * period of inactivity, in order to conserve battery power during such
+ * inactive periods.
+ *
+ * Typical actions are to turn on or off devices and adjust cpufreq parameters.
+ * This function may also call the appropriate interfaces to allow the kernel
+ * to suspend the system to low-power sleep state when entering non-interactive
+ * state, and to disallow low-power suspend when the system is in interactive
+ * state.  When low-power suspend state is allowed, the kernel may suspend the
+ * system whenever no wakelocks are held.
+ *
+ * on is non-zero when the system is transitioning to an interactive / awake
+ * state, and zero when transitioning to a non-interactive / asleep state.
+ *
+ * This function is called to enter non-interactive state after turning off the
+ * screen (if present), and called to enter interactive state prior to turning
+ * on the screen.
  */
 static void exynos5430_power_set_interactive(struct power_module *module, int on)
 {
@@ -242,89 +362,71 @@ static void exynos5430_power_set_interactive(struct power_module *module, int on
     char buf[80];
     int rc;
 
-    ALOGV("%s: Turn it %s\n", __func__, on ? "on" : "off");
-
-    /*
-     * Lower maximum frequency when screen is off.  CPU 0 and 1 share a
-     * cpufreq policy.
-     */
-    sysfs_write(CPU0_MAX_FREQ_PATH,
-                (!on || low_power_mode) ? LOW_POWER_MAX_FREQ : NORMAL_MAX_FREQ);
-
-    rc = stat(CPU0_MAX_FREQ_PATH, &sb);
-    if (rc == 0) {
-        sysfs_write(CPU0_MAX_FREQ_PATH,
-                    (!on || low_power_mode) ? LOW_POWER_MAX_FREQ : NORMAL_MAX_FREQ);
-    }
+    ALOGV("%s: Turn device %s\n", __func__, on ? "on" : "off");
 
     sysfs_write(exynos5430_pwr->touchscreen_power_path, on ? "1" : "0");
     sysfs_write(exynos5430_pwr->touchkey_power_path, on ? "1" : "0");
     sysfs_write(exynos5430_pwr->gpio_keys_power_path, on ? "1" : "0");
 }
 
-static struct timespec timespec_diff(struct timespec lhs, struct timespec rhs)
-{
-    struct timespec result;
-    if (rhs.tv_nsec > lhs.tv_nsec) {
-        result.tv_sec = lhs.tv_sec - rhs.tv_sec - 1;
-        result.tv_nsec = NSEC_PER_SEC + lhs.tv_nsec - rhs.tv_nsec;
-    } else {
-        result.tv_sec = lhs.tv_sec - rhs.tv_sec;
-        result.tv_nsec = lhs.tv_nsec - rhs.tv_nsec;
-    }
-    return result;
-}
-
-static int check_boostpulse_on(struct timespec diff)
-{
-    long boost_ns = (BOOST_PULSE_DURATION * NSEC_PER_USEC) % NSEC_PER_SEC;
-    long boost_s = BOOST_PULSE_DURATION / USEC_PER_SEC;
-
-    if (diff.tv_sec == boost_s)
-        return (diff.tv_nsec < boost_ns);
-    return (diff.tv_sec < boost_s);
-}
-
-/* You need to request the powerhal lock before calling this function */
-static int boostpulse_open(struct exynos5430_power_module *exynos5430_pwr)
-{
-    char errno_str[64];
-
-    if (exynos5430_pwr->boostpulse_fd < 0) {
-        exynos5430_pwr->boostpulse_fd = open(BOOSTPULSE_PATH, O_WRONLY);
-        if (exynos5430_pwr->boostpulse_fd < 0) {
-            if (!exynos5430_pwr->boostpulse_warned) {
-                strerror_r(errno, errno_str, sizeof(errno_str));
-                ALOGE("Error opening %s: %s\n", BOOSTPULSE_PATH, errno_str);
-                exynos5430_pwr->boostpulse_warned = 1;
-            }
-        }
-    }
-
-    return exynos5430_pwr->boostpulse_fd;
-}
-
 /*
- * This functions is called to pass hints on power requirements, which may
- * result in adjustment of power/performance parameters of the cpufreq governor
- * and other controls.
+ * The powerHint function is called to pass hints on power requirements, which
+ * may result in adjustment of power/performance parameters of the cpufreq
+ * governor and other controls.
+ *
+ * The possible hints are:
+ *
+ * POWER_HINT_VSYNC
+ *
+ *     Foreground app has started or stopped requesting a VSYNC pulse
+ *     from SurfaceFlinger.  If the app has started requesting VSYNC
+ *     then CPU and GPU load is expected soon, and it may be appropriate
+ *     to raise speeds of CPU, memory bus, etc.  The data parameter is
+ *     non-zero to indicate VSYNC pulse is now requested, or zero for
+ *     VSYNC pulse no longer requested.
+ *
+ * POWER_HINT_INTERACTION
+ *
+ *     User is interacting with the device, for example, touchscreen
+ *     events are incoming.  CPU and GPU load may be expected soon,
+ *     and it may be appropriate to raise speeds of CPU, memory bus,
+ *     etc.  The data parameter is unused.
+ *
+ * POWER_HINT_LOW_POWER
+ *
+ *     Low power mode is activated or deactivated. Low power mode
+ *     is intended to save battery at the cost of performance. The data
+ *     parameter is non-zero when low power mode is activated, and zero
+ *     when deactivated.
+ *
+ * POWER_HINT_CPU_BOOST
+ *
+ *     An operation is happening where it would be ideal for the CPU to
+ *     be boosted for a specific duration. The data parameter is an
+ *     integer value of the boost duration in microseconds.
  */
 static void exynos5430_power_hint(struct power_module *module,
                                   power_hint_t hint,
                                   void *data)
 {
-    struct exynos5430_power_module *exynos5430_pwr = (struct exynos5430_power_module *) module;
-    char errno_str[64];
-    int len;
+    struct exynos5430_power_module *exynos5430_pwr =
+        (struct exynos5430_power_module *)module;
 
     switch (hint) {
-        case POWER_HINT_INTERACTION:
+        case POWER_HINT_INTERACTION: {
+            char errno_str[64];
+            ssize_t len;
+            int fd;
+
+            if (current_power_profile == PROFILE_POWER_SAVE) {
+                return;
+            }
 
             ALOGV("%s: POWER_HINT_INTERACTION", __func__);
 
             pthread_mutex_lock(&exynos5430_pwr->lock);
-            if (boostpulse_open(exynos5430_pwr) >= 0) {
-
+            fd = boostpulse_open(exynos5430_pwr);
+            if (fd >= 0) {
                 len = write(exynos5430_pwr->boostpulse_fd, "1", 1);
                 if (len < 0) {
                     strerror_r(errno, errno_str, sizeof(errno_str));
@@ -338,7 +440,7 @@ static void exynos5430_power_hint(struct power_module *module,
             pthread_mutex_unlock(&exynos5430_pwr->lock);
 
             break;
-
+        }
         case POWER_HINT_VSYNC: {
             struct timespec now, diff;
 
@@ -372,28 +474,30 @@ static void exynos5430_power_hint(struct power_module *module,
             pthread_mutex_unlock(&exynos5430_pwr->lock);
             break;
         }
+        case POWER_HINT_SET_PROFILE: {
+            int profile = *((intptr_t *)data);
+
+            ALOGV("%s: POWER_HINT_SET_PROFILE", __func__);
+
+            set_power_profile(profile);
+            break;
+        }
         case POWER_HINT_LOW_POWER: {
             int rc;
             struct stat sb;
+            int low_power = *((intptr_t *)data);
 
             ALOGV("%s: POWER_HINT_LOW_POWER", __func__);
 
             pthread_mutex_lock(&exynos5430_pwr->lock);
 
-            if (data) {
-                sysfs_write(CPU0_MAX_FREQ_PATH, LOW_POWER_MAX_FREQ);
-                rc = stat(CPU4_MAX_FREQ_PATH, &sb);
-                if (rc == 0) {
-                    sysfs_write(CPU4_MAX_FREQ_PATH, LOW_POWER_MAX_FREQ);
-                }
+            if (low_power == 1) {
+                set_power_profile(PROFILE_POWER_SAVE);
             } else {
-                sysfs_write(CPU0_MAX_FREQ_PATH, NORMAL_MAX_FREQ);
-                rc = stat(CPU4_MAX_FREQ_PATH, &sb);
-                if (rc == 0) {
-                    sysfs_write(CPU4_MAX_FREQ_PATH, NORMAL_MAX_FREQ);
-                }
+                set_power_profile(PROFILE_BALANCED);
             }
-            low_power_mode = data;
+            /* FIXME */
+            low_power_mode = (bool)low_power;
 
             pthread_mutex_unlock(&exynos5430_pwr->lock);
             break;
@@ -403,28 +507,39 @@ static void exynos5430_power_hint(struct power_module *module,
     }
 }
 
+static int exynos5430_get_feature(struct power_module *module __unused,
+                                  feature_t feature)
+{
+    if (feature == POWER_FEATURE_SUPPORTED_PROFILES) {
+        return 3;
+    }
+
+    return -1;
+}
+
 static struct hw_module_methods_t power_module_methods = {
     .open = NULL,
 };
 
 struct exynos5430_power_module HAL_MODULE_INFO_SYM = {
-    base: {
-        common: {
-            tag: HARDWARE_MODULE_TAG,
-            module_api_version: POWER_MODULE_API_VERSION_0_2,
-            hal_api_version: HARDWARE_HAL_API_VERSION,
-            id: POWER_HARDWARE_MODULE_ID,
-            name: "EXYNOS5430 Power HAL",
-            author: "The Android Open Source Project",
-            methods: &power_module_methods,
+    .base = {
+        .common = {
+            .tag = HARDWARE_MODULE_TAG,
+            .module_api_version = POWER_MODULE_API_VERSION_0_2,
+            .hal_api_version = HARDWARE_HAL_API_VERSION,
+            .id = POWER_HARDWARE_MODULE_ID,
+            .name = "EXYNOS5430 Power HAL",
+            .author = "The CyanogenMod Project",
+            .methods = &power_module_methods,
         },
 
-        init: exynos5430_power_init,
-        setInteractive: exynos5430_power_set_interactive,
-        powerHint: exynos5430_power_hint,
+        .init = exynos5430_power_init,
+        .setInteractive = exynos5430_power_set_interactive,
+        .powerHint = exynos5430_power_hint,
+        .getFeature = exynos5430_get_feature
     },
 
-    lock: PTHREAD_MUTEX_INITIALIZER,
-    boostpulse_fd: -1,
-    boostpulse_warned: 0,
+    .lock = PTHREAD_MUTEX_INITIALIZER,
+    .boostpulse_fd = -1,
+    .boostpulse_warned = 0,
 };
