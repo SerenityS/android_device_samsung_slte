7d088731a5219aaf1223b84de363baa10ff13346
Andreas Schneider <asn@cryptomilk.org>
2016-04-26 19:57:53 +0200

    board: Update graphics board flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,6 +77,16 @@ BOARD_HDMI_INCAPABLE := true
 # less). Use 3 to avoid timing issues.
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
 # GSC
 #BOARD_USES_ONLY_GSC0_GSC1 := true
 
