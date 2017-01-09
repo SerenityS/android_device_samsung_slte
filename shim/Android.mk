162be1d5731f000e640a2da65c578836f67959bd
Andreas Schneider <asn@cryptomilk.org>
2017-01-09 08:11:02 +0100

    WIP: shim: Add missing functions for widevine

--- shim/Android.mk
+++ shim/Android.mk
@@ -34,3 +34,13 @@ LOCAL_MODULE := libshim_gpsd
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+## WVM
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := wvm.cpp
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation libmedia
+LOCAL_MODULE := libshim_wvm
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
