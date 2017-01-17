769cc91103d833d359b0aee17538a5cfc2576bf9
Andreas Schneider <asn@cryptomilk.org>
2017-01-17 10:55:08 +0100

    lineage: Fix installing apn-conf.xml from vendor/cm

--- lineage.mk
+++ lineage.mk
@@ -1,8 +1,8 @@
-# Inherit device configuration
-$(call inherit-product, device/samsung/sltexx/device.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/sltexx/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_sltexx
