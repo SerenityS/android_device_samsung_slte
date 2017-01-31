0dc142a58f1e5c7dac96be59af4f4cb819e9f111
Andreas Schneider <asn@cryptomilk.org>
2017-01-31 11:21:08 +0100

    config: Add mixer_paths_0.xml for unified samsung audio hal

--- device.mk
+++ device.mk
@@ -146,13 +146,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
 	$(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+	$(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
 
 PRODUCT_PACKAGES += \
 	audio.a2dp.default \
 	audio.usb.default \
 	audio.r_submix.default \
-	audio.primary.universal5430
+	audio.primary.universal5430 \
+	libtinycompress
 
 ###########################################################
 ### OMX/MEDIA
