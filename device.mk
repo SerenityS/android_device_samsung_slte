41282e3a3763916ed2b24cf84b36e81a1dfc6831
Andreas Schneider <asn@cryptomilk.org>
2017-02-09 17:34:09 +0100

    Revert "device: Build with su by default"
    This reverts commit 6cb90a785a17996964429b49ac10e1d5e9ca2285.

--- device.mk
+++ device.mk
@@ -269,8 +269,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
 	SamsungServiceMode \
-	Torch \
-	su
+	Torch
 
 $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product-if-exists, build/target/product/full.mk)
