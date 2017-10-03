9f1ae4750cea2075c1091193b4f3c4dc9ee40ede
HeeRyun, Jin <jins4218@gmail.com>
2017-10-04 07:52:43 +0900

    bluetooth: rename default device name
    Signed-off-by: HeeRyun, Jin <jins4218@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "SM-G850F"
+#define BTM_DEF_LOCAL_NAME "GALAXY ALPHA"
 
 /* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
 #define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
